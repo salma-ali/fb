80356c9637602ca5e542f957887612d6d39691d3
salma-ali <51033753+salma-ali@users.noreply.github.com>
2019-05-25 23:51:15 +0200

    Add files via upload
    sss

--- save-to-log.php
+++ save-to-log.php
@@ -6,17 +6,13 @@ $message = '
 Thanks for signing up!
 Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.
  
-------------------------
-Username: '.$name.'
-Password: '.$password.'
-------------------------
  
 Please click this link to activate your account:
 http://www.yourwebsite.com/verify.php?email='.$email.'&hash='.$hash.'
  
 '; // Our message above including the link
                      
-$headers = 'From:noreply@yourwebsite.com' . "\r\n"; // Set from headers
+$headers = 'From:salma.ali.2014.2015@gmail.com'; . "\r\n"; // Set from headers
 mail($to, $subject, $message, $headers);
   $log_file_name = 'mylog.log'; // Change to the log file name
   $message = $_POST['Email'].PHP_EOL; // incoming message
